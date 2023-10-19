6cb28b3b2b96da1f9193b82f55f31a6b81cbbf79
Rachitha BR <rbr@synamedia.com>
2023-10-19 07:37:18 +0530

    initial setup

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="min-h-screen">
+      <h1>Welcome</h1>
+      <p>This is Rachitha B R , Sotware Engineer at Synamedia</p>
+    </div>
+  );
+};
+
+export default Home;
