7fcf8e868f3a454ea1a467d4a163b6c8f571294c
Rach1507 <rachitha715@gmail.com>
2023-10-28 23:07:39 +0530

    Experience card view changes

--- src/components/Experience/ExperienceCardView.js
+++ src/components/Experience/ExperienceCardView.js
@@ -1,36 +1,215 @@
+import { useState } from "react";
+
 export default function ExperienceCardView() {
+  const [selected, setSelected] = useState("FE");
   return (
     <div className="basis-4/5">
-      {/* just align-items to center x instead of padding - check  */}
-      <div className="p-36 pl-0  flex-auto flex-row items-start ">
+      <div className="p-36 pl-0 flex-auto flex-row items-start ">
         <span className="text-4xl font-sans font-bold bg-clip-text tracking-wide text-transparent bg-gradient-to-l from-slate-500 to-slate-200">
           Experience
         </span>
-        <br />
-        <div className="grid grid-cols-3 mt-36">
-          <div class="grid col-start-1 grid-rows-3">
-            <button className=" p-5 hover:border-l hover:accent-transparent">
+        <div className="grid grid-cols-6 mt-24">
+          <div class="grid col-start-1 grid-rows-5 ">
+            <button
+              className=" p-5 hover:border-l  hover:backdrop-contrast-75"
+              onClick={() => {
+                setSelected("FE");
+              }}
+            >
               Jan 2023 - Present
             </button>
-            <button className=" p-5 hover:border-l">
+            <button
+              className=" p-5 hover:border-l  hover:backdrop-contrast-75 "
+              onClick={() => {
+                setSelected("BE");
+              }}
+            >
               June 2022 - Dec 2022
             </button>
-            <button className=" p-5 hover:border-l">
+            <button
+              className=" p-5 hover:border-l  hover:backdrop-contrast-75"
+              onClick={() => {
+                setSelected("OT");
+              }}
+            >
               Mar 2022 - June 2022
             </button>
           </div>
-          <div className="col-span-2 "></div>
+          {selected === "FE" && (
+            <div className="col-span-5 col-start-2 ml-36">
+              <div className="grid grid-rows-1 grid-cols-3">
+                <div className="border-t-4  border-double "></div>
+                <div className="grid-start-1 col-span-3 text-xl z-20 border-l-4  border-double pt-10 pl-10 h-24">
+                  Associate Software Engineer (Front End) - Synamedia
+                </div>
+              </div>
+
+              <div className=" ml-24 p-5 z-10 mr-24">
+                <ul class="list-disc">
+                  <li className="p-3">
+                    Now this is a story all about how, my life got
+                    flipped-turned upside down.Now this is a story all about
+                    how, my life got flipped-turned upside down
+                  </li>
+                  <li className="p-3">
+                    Now this is a story all about how, my life got
+                    flipped-turned upside down.Now this is a story all about
+                    how, my life got flipped-turned upside down
+                  </li>
+                  <li className="p-3">
+                    Now this is a story all about how, my life got
+                    flipped-turned upside down.Now this is a story all about
+                    how, my life got flipped-turned upside down
+                  </li>
+                  <li className="p-3">
+                    Now this is a story all about how, my life got
+                    flipped-turned upside downNow this is a story all about how,
+                    my life got flipped-turned upside down
+                  </li>
+                  <li className="p-3 flex flex-row gap-4">
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      ReactJS
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      NextJS
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      Sass
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      Headless
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      Cypress
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      Style Dictionary
+                    </button>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="grid grid-row-1 grid-cols-3">
+                <div className="grid-start-1 col-span-3 text-xl z-20 border-r-4  border-double pt-10 pl-10 h-24" />
+                <div className="border-b-4 col-start-3 border-double "></div>
+              </div>
+            </div>
+          )}
+
+          {selected === "BE" && (
+            <div className="col-span-4 col-start-2 ml-36">
+              <div className="grid grid-rows-1 grid-cols-3">
+                <div className="border-t-4  border-double "></div>
+                <div className="grid-start-1 col-span-3 text-xl z-20 border-l-4  border-double pt-10 pl-10 h-24">
+                  Associate Software Engineer (Back End) - Synamedia
+                </div>
+              </div>
+
+              <div className=" p-24 z-10">
+                <ul class="list-disc">
+                  <li className="">
+                    Now this is a story all about how, my life got
+                    flipped-turned upside down.Now this is a story all about
+                    how, my life got flipped-turned upside down
+                  </li>
+                  <li className="p-3">
+                    Now this is a story all about how, my life got
+                    flipped-turned upside down.Now this is a story all about
+                    how, my life got flipped-turned upside down
+                  </li>
+                  <li className="p-3">
+                    Now this is a story all about how, my life got
+                    flipped-turned upside down.Now this is a story all about
+                    how, my life got flipped-turned upside down
+                  </li>
+                  <li className="p-3">
+                    Now this is a story all about how, my life got
+                    flipped-turned upside downNow this is a story all about how,
+                    my life got flipped-turned upside down
+                  </li>
+                  <li className="p-3 flex flex-row gap-4">
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      ReactJS
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      NextJS
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      Sass
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      Headless
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      Cypress
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      Style Dictionary
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+
+          {selected === "OT" && (
+            <div className="col-span-4 col-start-2 ml-36">
+              <div className="grid grid-rows-1 grid-cols-3">
+                <div className="border-t-4  border-double "></div>
+                <div className="grid-start-1 col-span-3 text-xl z-20 border-l-4  border-double pt-10 pl-10 h-24">
+                  Full Stack Developer Intern - OneTrust
+                </div>
+              </div>
+
+              <div className=" ml-24 p-5 z-10">
+                <ul class="list-disc">
+                  <li className="p-3">
+                    Now this is a story all about how, my life got
+                    flipped-turned upside down.Now this is a story all about
+                    how, my life got flipped-turned upside down
+                  </li>
+                  <li className="p-3">
+                    Now this is a story all about how, my life got
+                    flipped-turned upside down.Now this is a story all about
+                    how, my life got flipped-turned upside down
+                  </li>
+                  <li className="p-3">
+                    Now this is a story all about how, my life got
+                    flipped-turned upside down.Now this is a story all about
+                    how, my life got flipped-turned upside down
+                  </li>
+                  <li className="p-3">
+                    Now this is a story all about how, my life got
+                    flipped-turned upside downNow this is a story all about how,
+                    my life got flipped-turned upside down
+                  </li>
+                  <li className="p-3 flex flex-row gap-4">
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      ReactJS
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      NextJS
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      Sass
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      Headless
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      Cypress
+                    </button>
+                    <button className="rounded-full bg-sky-200 p-2 text-xs text-black">
+                      Style Dictionary
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
+    // </div>
   );
-  //     <div className="grid grid-cols-3">
-  //       <div class="grid grid-rows-3 divide-y hover:divide-sky-500">
-  //         <div className="text-center p-5">01</div>
-  //         <div>02</div>
-  //         <div>03</div>
-  //       </div>
-  //       <div ></div>
-  //     </div>
-  //   );
 }
