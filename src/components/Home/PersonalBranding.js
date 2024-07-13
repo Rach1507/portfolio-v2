88ac0323f78530548bfb018679dadd7b199e96c2
Rach1507 <rachitha715@gmail.com>
2024-07-13 07:08:15 +0530

    Update Person Branding Page

--- src/components/Home/PersonalBranding.js
+++ src/components/Home/PersonalBranding.js
@@ -18,26 +18,26 @@ const PersonalBranding = () => {
           <div className="h-4/5 w-full flex flex-col gap-12 items-start justify-center text-intro-desc">
             <Fade triggerOnce cascade className=" text-right w-full   ">
               <span
-                className="text-8xl
+                className="text-9xl
                 font-sans font-semibold tracking-wide
                 bg-clip-text  text-transparent
-                overflow-hidden whitespace-nowrap  bg-gradient-to-l from-slate-400 to-slate-50 
-                text-slate-300 "
+                overflow-hidden whitespace-nowrap bg-gradient-to-l from-slate-400 to-slate-50 
+                text-white "
               >
                 Rachitha B R
               </span>
               <div className="flex flex-col gap-12">
-                <span className="text-5xl  tracking-wide text-slate-300 font-semibold">
+                <span className="text-4xl tracking-wide text-slate-300 font-semibold mb-5">
                   {" "}
-                  <span className="text-sky-500"> www.rachithabr.dev </span>
+                  <span className="text-sky-50 bg-opacity-30 bg-slate-400  p-3"> www.rachithabr.dev </span>
                 </span>
-                <span className="text-5xl   text-slate-300 tracking-wide font-semibold">
+                <span className="text-4xl text-slate-300 tracking-wide font-semibold">
                   {" "}
                   {" "}
-                  <span className="text-sky-500">
+                  <button className="text-sky-50 bg-opacity-30 bg-slate-400 p-3">
                     {" "}
                     brrachitha027@gmail.com{" "}
-                  </span>{" "}
+                  </button>{" "}
                 </span>
               </div>
             </Fade>
